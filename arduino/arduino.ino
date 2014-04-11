5119858747017dd64e311161217800745762c349
Michal Cichra <michal@o2h.cz>
2014-04-11 18:57:24 +0200

    working prototype

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,15 +1,13 @@
-int timer = 60;
+int timer = 70;
 int segments[4];
 int max_segments = sizeof(segments)/sizeof(int);
 
 int current_segment = 0;
-int multiplex_rate = 100;
+int multiplex_rate = 400;
 int multiplex_delay = 1000 / multiplex_rate;
 bool ticking = true;
 
 void setup() {
-  Serial.begin(9600);
-  
   pinMode(0, OUTPUT);   
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
@@ -34,19 +32,27 @@ int next_segment() {
 }
 
 void loop() {
-
-
-  update();
-  
-  // multiplex loop
-  
-  multiplex();
+  if(timer <= 0) {
+    timer = 0;
+    update();
+    ticking = false;
+  }
   
   // subtract second and finish
   if (ticking) {
+    
+    update();
+  
+  // multiplex loop
+  
+    multiplex();
+  
     timer -= 1;
+  } else {
+    multiplex();
   }
 }
+
 
 void update() {
   int minutes = timer / 60;
@@ -56,12 +62,10 @@ void update() {
   segments[1] = minutes % 10;
   segments[2] = seconds / 10;
   segments[3] = seconds % 10;
-  
-  Serial.print("timer:");
-  Serial.println(timer);
 }
 
 void show_segment(int segment) {
+  fade_segments();
   show_number(segments[segment]);
   activate_segment(segment);
 }
@@ -75,77 +79,35 @@ void multiplex() {
   }
 }
 
+byte numbers[10][7] = {
+  { 1,1,1,1,1,1,0 },  // = 0
+  { 0,1,1,0,0,0,0 },  // = 1
+  { 1,1,0,1,1,0,1 },  // = 2
+  { 1,1,1,1,0,0,1 },  // = 3
+  { 0,1,1,0,0,1,1 },  // = 4
+  { 1,0,1,1,0,1,1 },  // = 5
+  { 1,0,1,1,1,1,1 },  // = 6
+  { 1,1,1,0,0,0,0 },  // = 7
+  { 1,1,1,1,1,1,1 },  // = 8
+  { 1,1,1,0,0,1,1 }   // = 9
+};
 
 void show_number(int number) {
-  Serial.print("Showing: ");
-  Serial.println(number);
-  
-  for(int i = 4; i <= 10; i++) {
-    digitalWrite(i, LOW);
+  for(int i=0; i < 7; i++) {
+    digitalWrite(i + 4, numbers[number][i]);
   }
-  
-  switch(number) {
-    case 0:
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
-      digitalWrite(9, HIGH);
-      break;
-    case 1:
-      digitalWrite(5, HIGH);
-      break;
-    case 2:
-      digitalWrite(6, HIGH);
-      break;
-    case 3:
-      digitalWrite(7, HIGH);
-      break;
-    case 4:
-      digitalWrite(8, HIGH);
-      break;
-    case 5:
-      digitalWrite(9, HIGH);
-      break;
-    case 6:
-      digitalWrite(10, HIGH);
-      break;
-    case 7:
-      digitalWrite(5, HIGH);
-      break;
-    case 8:
-      digitalWrite(6, HIGH);
-      break;
-    case 9:
-      digitalWrite(7, HIGH);
-      break;
-  }
+ 
 }
 
-
-void activate_segment(int segment) {
+void fade_segments() {
   digitalWrite(0, LOW);
   digitalWrite(1, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
-  
-  Serial.print("activating segment");
-  Serial.println(segment);
+}
+
+void activate_segment(int segment) {
   digitalWrite(segment, HIGH);
-  /**
-  switch(segment) {
-    case 0:
-      digitalWrite(segment, HIGH);
-      break;
-    case 1:
-      digitalWrite(0, HIGH);
-      break;
-    case 2:
-    digitalWrite(0, HIGH);
-      break;
-    case 3:
-      break;
-  } **/
+
 }
     
