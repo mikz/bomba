05cefbb2236f689f02601400b232dc091a9bf750
Michal Cichra <michal@o2h.cz>
2014-04-21 22:02:05 +0200

    use 6 digit display
    * also change how time is measured

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,113 +1,150 @@
-int timer = 70;
-int segments[4];
-int max_segments = sizeof(segments)/sizeof(int);
+const unsigned long timer = (30 * 60 + 50) * 1000; // time in ms
+unsigned long int remaining = timer;
+
+int segments = 6;
+int numbers[6];
+unsigned long boot;
+
+// enable Serial << streaming
+template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } 
+
+const byte segment_numbers[10][7] = {
+    { 1,1,1,1,1,1,0 },  // = 0
+    { 0,1,1,0,0,0,0 },  // = 1
+    { 1,1,0,1,1,0,1 },  // = 2
+    { 1,1,1,1,0,0,1 },  // = 3
+    { 0,1,1,0,0,1,1 },  // = 4
+    { 1,0,1,1,0,1,1 },  // = 5
+    { 1,0,1,1,1,1,1 },  // = 6
+    { 1,1,1,0,0,0,0 },  // = 7
+    { 1,1,1,1,1,1,1 },  // = 8
+    { 1,1,1,0,0,1,1 }   // = 9
+};
+
+const int segment_pins[6] = { 2, 3, 4, 5, 6 };
+const int number_pins[7] = { 7, 8, 9, 10, 11, 12, 13 };
+const int button_pins[5] = { A0, A1, A2, A3, A4 };
+const int buttons = sizeof(button_pins)/sizeof(int);
+const int siren_pin = A5;
 
 int current_segment = 0;
-int multiplex_rate = 400;
-int multiplex_delay = 1000 / multiplex_rate;
+const int multiplex_rate = 400;
+const int multiplex_delay = 1000 / multiplex_rate;
+
 bool ticking = true;
+bool started = true;
 
 void setup() {
-  pinMode(0, OUTPUT);   
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-}
+    Serial.begin(9600);
+    
+    boot = millis();
+    
+    for(int i=0; i < segments; i++) {
+      pinMode(segment_pins[i], OUTPUT);
+    }
 
-int next_segment() {
-  int next_segment = current_segment + 1;
-  if (next_segment >= max_segments) {
-    return 0;
-  } else {
-    return next_segment;
-  }
+    for(int i=0; i < 7; i++) {
+      pinMode(number_pins[i], OUTPUT);
+    }
 }
 
 void loop() {
-  if(timer <= 0) {
-    timer = 0;
-    update();
-    ticking = false;
-  }
-  
-  // subtract second and finish
-  if (ticking) {
-    
-    update();
-  
-  // multiplex loop
-  
-    multiplex();
-  
-    timer -= 1;
-  } else {
-    multiplex();
-  }
+  if(started)
+    tick();
 }
 
+void tick() {
+    if(remaining <= 0) {
+        remaining = 0;
+        update();
+        ticking = false;
+    }
+
+    if (ticking) {
+        
+        update();
+        
+        // multiplex loop
+        
+        multiplex();
+    } else {
+        multiplex();
+        beep();
+    }
+}
+
+int next_segment() {
+    int next_segment = current_segment + 1;
+    if (next_segment >= segments) {
+        return 0;
+    } else {
+        return next_segment;
+    }
+}
+
+void update_clock(int first, int second, int third) {
+    Serial << first << ":" << second << ":" << third << "\n";
+    numbers[0] = first / 10;
+    numbers[1] = first % 10;
+    numbers[2] = second / 10;
+    numbers[3] = second % 10;
+    numbers[4] = third / 10;
+    numbers[5] = third % 10;
+}
+
+unsigned long elapsed() {
+  return millis() - boot;
+}
 
 void update() {
-  int minutes = timer / 60;
-  int seconds = timer % 60;
-  
-  segments[0] = minutes / 10;
-  segments[1] = minutes % 10;
-  segments[2] = seconds / 10;
-  segments[3] = seconds % 10;
+  Serial.println(elapsed());
+    remaining = timer - elapsed();
+
+    int miliseconds = remaining % 1000;
+    int seconds_left = remaining / 1000;
+    
+    int hours  =  seconds_left / 3600;
+    int minutes = seconds_left / 60;
+    int seconds = seconds_left % 60;
+    
+    if (hours > 0) { // show hours:minutes:seconds
+      update_clock(hours, minutes, seconds);
+    } else { // show minutes:seconds:miliseconds 
+      update_clock(minutes, seconds, miliseconds / 100);
+    }
 }
 
 void show_segment(int segment) {
-  fade_segments();
-  show_number(segments[segment]);
-  activate_segment(segment);
+    fade_segments();
+    show_number(numbers[segment]);
+    activate_segment(segment);
 }
 
 
 void multiplex() {
-  for(int i=0; i < multiplex_rate; i++) {    
-    show_segment(current_segment);
-    current_segment = next_segment();
-    delay(multiplex_delay);
-  }
+    for(int i=0; i < multiplex_rate; i++) {
+        show_segment(current_segment);
+        current_segment = next_segment();
+        delay(multiplex_delay);
+    }
 }
 
-byte numbers[10][7] = {
-  { 1,1,1,1,1,1,0 },  // = 0
-  { 0,1,1,0,0,0,0 },  // = 1
-  { 1,1,0,1,1,0,1 },  // = 2
-  { 1,1,1,1,0,0,1 },  // = 3
-  { 0,1,1,0,0,1,1 },  // = 4
-  { 1,0,1,1,0,1,1 },  // = 5
-  { 1,0,1,1,1,1,1 },  // = 6
-  { 1,1,1,0,0,0,0 },  // = 7
-  { 1,1,1,1,1,1,1 },  // = 8
-  { 1,1,1,0,0,1,1 }   // = 9
-};
-
 void show_number(int number) {
-  for(int i=0; i < 7; i++) {
-    digitalWrite(i + 4, numbers[number][i]);
-  }
- 
+    for(int i=0; i < 7; i++) {
+        digitalWrite(i + 4, segment_numbers[number][i]);
+    }
+   
 }
 
 void fade_segments() {
-  digitalWrite(0, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
+    for(int i=0; i <  sizeof(segment_pins)/sizeof(int); i++) {
+        digitalWrite(segment_pins[i], LOW);
+    }
 }
 
 void activate_segment(int segment) {
-  digitalWrite(segment, HIGH);
-
+    digitalWrite(segment, HIGH);
 }
-    
+
+void beep() {
+}
