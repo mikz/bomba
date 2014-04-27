acb7bc8ce17c1da6a3825ae4c1243642679de4b6
Michal Cichra <michal@o2h.cz>
2014-04-27 10:48:46 +0200

    turn off multiplex delay

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,33 @@
-const unsigned long timer = (30 * 60 + 50) * 1000; // time in ms
+// (minutes * 60 + seconds) * 1000 = time in miliseconds
+const unsigned long timer = (0 * 60 + 50) * 1000;
+
+// controls wether to print deubbging info to serial port 
+const bool print_pins = true;
+const bool print_buttons = true;
+
+// configuration of output/input pins
+const int segment_pins[6] = { 2, 3, 4, 5, 6 };
+const int number_pins[7] = { 7, 8, 9, 10, 11, 12, 13 };
+const int button_pins[5] = { A0, A1, A2, A3, A4 };
+
+const int siren_pin = A5;
+
+// multiplex rate controls how many times to rotate between segments before updating displayed number
+const int multiplex_rate = 50;
+
+// help vars below
+bool ticking = true;
+bool started = true;
+
+const int buttons = sizeof(button_pins)/sizeof(int);
+const int segments = sizeof(segment_pins)/sizeof(int);
+
 unsigned long int remaining = timer;
 
-int segments = 6;
+int current_segment = 0;
+
 int numbers[6];
 unsigned long boot;
-
-// enable Serial << streaming
-template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } 
 
 const byte segment_numbers[10][7] = {
     { 1,1,1,1,1,1,0 },  // = 0
@@ -21,18 +42,8 @@ const byte segment_numbers[10][7] = {
     { 1,1,1,0,0,1,1 }   // = 9
 };
 
-const int segment_pins[6] = { 2, 3, 4, 5, 6 };
-const int number_pins[7] = { 7, 8, 9, 10, 11, 12, 13 };
-const int button_pins[5] = { A0, A1, A2, A3, A4 };
-const int buttons = sizeof(button_pins)/sizeof(int);
-const int siren_pin = A5;
-
-int current_segment = 0;
-const int multiplex_rate = 50;
-const int multiplex_delay = 1000 / multiplex_rate;
-
-bool ticking = true;
-bool started = true;
+// enable Serial << streaming
+template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } 
 
 void setup() {
     Serial.begin(9600);
@@ -102,7 +113,6 @@ unsigned long elapsed() {
 
 void update() {
     remaining = timer - elapsed();
-
     int miliseconds = remaining % 1000;
     int seconds_left = remaining / 1000;
     
@@ -135,10 +145,13 @@ void multiplex() {
     for(int i=0; i < multiplex_rate; i++) {
         show_segment(current_segment);
         current_segment = next_segment();
-        delay(multiplex_delay);
-for(int j=0; j < buttons; j++) {
-  Serial << "Button: " << j << " = " << read_button(j) << "\n";
-}
+
+        for(int j=0; j < buttons; j++) {
+		int button_value = read_button(j);
+		if (print_buttons) {
+			Serial << "Button: " << j << " = " << button_value << "\n";
+		}
+        }
     }
 };
 
@@ -159,12 +172,16 @@ void fade_segments() {
 
 void turn_on(int pin) {
   digitalWrite(pin, HIGH);
-  Serial << "Turn ON pin: " << pin << "\n";
+  if (print_pins) {
+      Serial << "Turn ON pin: " << pin << "\n";
+  }
 };
 
 void turn_off(int pin) {
   digitalWrite(pin, LOW);
-  Serial << "Turn OFF pin: " << pin << "\n";
+  if (print_pins) {
+      Serial << "Turn OFF pin: " << pin << "\n";
+  }
 };
 
 void beep() {
