b15aa3eee7d78e045160d73f57ca61dfe100237a
Michal Cichra <mikz@users.noreply.github.com>
2014-04-29 18:25:00 +0200

    Update arduino.ino

--- src/arduino.ino
+++ src/arduino.ino
@@ -184,7 +184,7 @@ void blink() {
 
 void show_number(int number) {
     Serial << "Showing number: " << number << "\n";
-    for(int i=0; i < segments; i++) {
+    for(int i=0; i < numbers; i++) {
       int pin = number_pins[i];
       bool on = segment_numbers[number][i];
       on ? turn_on(pin) : turn_off(pin);
